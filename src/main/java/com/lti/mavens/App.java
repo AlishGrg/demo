304fe7680e9fef7a731ef2163f60317b84f34886
AlishGrg <alish.grg04@gmail.com>
2021-09-15 15:42:45 +0530

    commit

--- src/main/java/com/lti/mavens/App.java
+++ src/main/java/com/lti/mavens/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hy World! I am checking GIT commands. Let's see how it works!" );
+        System.out.println( "Hy People, I am checking GIT commands. Let's see how it works!" );
     }
 }
